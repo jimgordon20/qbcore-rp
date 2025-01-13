4ae746ce291d75e5e858945c57eb26f71d73ef57
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2025-01-13 15:16:01 -0600

    qb-taxijob

---
+++ Packages/qb-taxijob/Server/Index.lua
@@ -0,0 +1,3 @@
+Events.SubscribeRemote('qb-busjob:server:spawnTaxi', function()
+    HSimpleVehicle(Vector(22689.3, 100016.9, 98.5), Rotator(-0.24469050765038, -90.378021240234, 0.03790046274662), 'abcca-qbcore-vehicles::BP_Taxi')
+end)
