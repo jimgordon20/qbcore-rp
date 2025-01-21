e189f764b18efe654b84ec7cf8f4218881c57355
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2025-01-21 12:59:00 -0600

    update core-ui

--- Packages/core-ui/Server/Index.lua
+++ Packages/core-ui/Server/Index.lua
@@ -7,18 +7,3 @@ Events.SubscribeRemote('SetInputEnabled', function(player, enabled)
         hcharacter:SetInputEnabled(enabled)
     end
 end)
-
--- Uncomment this to spawn a character for each player (for testing purposes)
--- Package.Subscribe("Load", function()
---     local allPlayers = Player.GetAll()
---     for _, player in ipairs(allPlayers) do
---         my_hcharacter = HCharacter(Vector(0, 0, 0), Rotator(0, 0, 0), player)
---         player:Possess(my_hcharacter)
---     end
--- end)
-
--- Uncomment this to spawn a character for each player (for testing purposes)
--- Player.Subscribe("Ready", function (self)
---     local my_hcharacter = HCharacter(Vector(0, 0, 0), Rotator(0, 0, 0), self)
---     self:Possess(my_hcharacter)
--- end)
