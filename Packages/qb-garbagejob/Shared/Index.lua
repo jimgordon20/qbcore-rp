ca415b649bb87402f1fdc08fb6242f8d8f8377b4
Joel-Hypersonic <joel@hypersoniclabs.co>
2025-01-28 14:53:49 +0000

    refactor(garbage): simplify config, use single quotes

--- Packages/qb-garbagejob/Shared/Index.lua
+++ Packages/qb-garbagejob/Shared/Index.lua
@@ -1,15 +1,16 @@
 Config = {
+    Vehicle = 'BP_Garbage_Truck',
+
     Locations = {
-        ["Depots"] = {
+        ['Depots'] = {
             {
-                blip = Vector(0, 0, 0),
-                label = "West Garbage Depot",
-                ped = Vector(0, 0, 0),
-                vehicleSpawn = Vector(0, 0, 0),
-            }
+                label = 'West Garbage Depot',
+                pedSpawn = { coords = Vector(-257770.4, 35765.8, 184.7), heading = -4.25 },
+                vehicleSpawn = { coords = Vector(0, 0, 0), heading = 0.0 },
+            },
         },
 
-        ["Dumpsters"] = {
+        ['Dumpsters'] = {
             { coords = Vector(-254486.1, 36299.2, 188.8), heading = -170.6 },
         },
     },
