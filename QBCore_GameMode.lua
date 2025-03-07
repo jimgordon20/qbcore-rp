036564b7b5a45b3e7a2e7195ea862ca25c253c86
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2025-03-07 16:09:20 -0600

    General cleanup

--- QBCore_GameMode.lua
+++ QBCore_GameMode.lua
@@ -1,32 +1,13 @@
---
--- DESCRIPTION
---
--- @COMPANY **
--- @AUTHOR **
--- @DATE ${date} ${time}
---
-
 ---@type QBCore_GameMode_C
 local M = UnLua.Class()
 
 function M:K2_PostLogin(NewPlayerController)
     self.Overridden.K2_PostLogin(self, NewPlayerController)
-
     QBCore.Player.Login(NewPlayerController, 'qwertyCitizenId')
 end
 
--- function M:Initialize(Initializer)
--- end
-
--- function M:UserConstructionScript()
--- end
-
 function M:ReceiveBeginPlay()
     --print('QBCore_GameMode_C:ReceiveBeginPlay')
-end
-
-function M:K2_PostLogin(NewPlayerController)
-    print('QBCore_GameMode_C:K2_PostLogin', NewPlayerController)
 end
 
 function M:K2_OnLogout()
@@ -36,17 +17,5 @@ end
 function M:ReceiveEndPlay()
     --print('QBCore_GameMode_C:ReceiveEndPlay')
 end
-
--- function M:ReceiveTick(DeltaSeconds)
--- end
-
--- function M:ReceiveAnyDamage(Damage, DamageType, InstigatedBy, DamageCauser)
--- end
-
--- function M:ReceiveActorBeginOverlap(OtherActor)
--- end
-
--- function M:ReceiveActorEndOverlap(OtherActor)
--- end
 
 return M
