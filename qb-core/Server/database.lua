db136f4bb26462876d35c6cc5ca0e3ffdf2b0080
Joel-Hypersonic (Qwerty) <194819701+Joel-Hypersonic@users.noreply.github.com>
2025-11-05 19:20:21 -0500

    refactor(core/db): automatically return table of results

--- qb-core/Server/database.lua
+++ qb-core/Server/database.lua
@@ -406,7 +406,7 @@ function DatabaseAction(ActionType, ...)
             ResultSet[k] = v.Columns:ToTable()
         end
     end
-    return #ResultSet ~= 0 and ResultSet or result
+    return #ResultSet ~= 0 and ResultSet or result:ToTable()
 end
 exports('qb-core', 'DatabaseAction', DatabaseAction)
 
