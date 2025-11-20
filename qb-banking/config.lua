c310f197fc7037e8bc6c4a4a1f65f5864e693dcb
Kakarot <joshuaeger@hypersoniclabs.co>
2025-11-20 16:08:24 -0600

    add coord

--- qb-banking/config.lua
+++ qb-banking/config.lua
@@ -2,6 +2,7 @@ Config = {
     atmModels = { 'SM_ATM' },
     maxAccounts = 2,
     locations = {
+        Vector(-338098, -141936, -2687),
         Vector(-337837, -141951, -2687),
         Vector(-337719, -141556, -2687),
         Vector(-337734, -141287, -2687),
