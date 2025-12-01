ad4cd5fbd0f0ea7b2adbd4a008869f42c95633ee
Kakarot <joshuaeger@hypersoniclabs.co>
2025-12-01 17:55:57 -0600

    taxi job updates

--- qb-taxijob/client.lua
+++ qb-taxijob/client.lua
@@ -1,5 +1,13 @@
 local Lang = require('locales/en')
 local my_webui = WebUI('qb-taxijob', 'qb-taxijob/html/index.html')
+local meterOpen = false
+local meterActive = false
+local inPickupZone = false
+local inDropoffZone = false
+local currentPickupBenchIndex = nil
+local currentDropoffBenchIndex = nil
+local pickupZone = nil
+local dropoffZone = nil
 
 -- UI Events
 
@@ -58,38 +66,86 @@ end)
 
 RegisterClientEvent('qb-taxijob:client:toggleMeter', function()
     local vehicle = GetVehiclePedIsIn(GetPlayerPawn())
-    local model = vehicle.Object
-    local modelName = model:GetName()
-    print(modelName)
+    if not vehicle then return end
+    if not meterOpen then
+        my_webui:SendEvent('openMeter', { toggle = true, meterData = Config.Meter })
+        meterOpen = true
+    else
+        my_webui:SendEvent('openMeter', { toggle = false })
+        meterOpen = false
+    end
 end)
 
 RegisterClientEvent('qb-taxijob:client:enableMeter', function()
     local vehicle = GetVehiclePedIsIn(GetPlayerPawn())
-    local model = vehicle.Object
-    local modelName = model:GetName()
-    print(modelName)
+    if not vehicle then return end
+    if meterOpen then
+        my_webui:SendEvent('toggleMeter')
+    end
 end)
 
-local inZone = false
-local currentBenchIndex = nil
-
 RegisterClientEvent('qb-taxijob:client:pickupSpot', function(coords, benchIndex)
-    currentBenchIndex = benchIndex
     coords.X = coords.X + 300
-    local CurrentZone = Trigger(coords, Rotator(), Vector(100), TriggerType.Sphere, true, function()
-        inZone = true
+    if pickupZone then
+        DeleteEntity(pickupZone)
+    end
+
+    pickupZone = Trigger(coords, Rotator(), Vector(100), TriggerType.Sphere, true, function()
+        inPickupZone = true
+        currentPickupBenchIndex = benchIndex
         exports['qb-core']:DrawText('[E] - Pickup NPC', 'left')
     end)
-    local Shape = CurrentZone:K2_GetComponentByClass(UE.UShapeComponent)
+
+    local Shape = pickupZone:GetComponentByClass(UE.UShapeComponent)
     Shape.OnComponentEndOverlap:Add(HWorld, function(_)
-        inZone = false
+        inPickupZone = false
+        currentPickupBenchIndex = nil
         exports['qb-core']:HideText()
     end)
+end)
+
+RegisterClientEvent('qb-taxijob:client:dropoffSpot', function(coords, benchIndex)
+    if pickupZone then
+        DeleteEntity(pickupZone)
+        pickupZone = nil
+    end
+    inPickupZone = false
+    currentPickupBenchIndex = nil
+    exports['qb-core']:HideText()
+    dropoffZone = Trigger(coords, Rotator(), Vector(100), TriggerType.Sphere, true, function()
+        inDropoffZone = true
+        currentDropoffBenchIndex = benchIndex
+        exports['qb-core']:DrawText('[E] - Drop Off NPC', 'left')
+    end)
+
+    local Shape = dropoffZone:GetComponentByClass(UE.UShapeComponent)
+    Shape.OnComponentEndOverlap:Add(HWorld, function(_)
+        inDropoffZone = false
+        currentDropoffBenchIndex = nil
+        exports['qb-core']:HideText()
+    end)
+end)
+
+RegisterClientEvent('qb-taxijob:client:jobComplete', function()
+    if dropoffZone then
+        DeleteEntity(dropoffZone)
+        dropoffZone = nil
+    end
+    inDropoffZone = false
+    currentDropoffBenchIndex = nil
+    exports['qb-core']:HideText()
+
+    exports['qb-core']:Notify('Job complete! Great work!', 'success')
 end)
 
 -- Input
 
 Input.BindKey('E', function()
-    if not inZone then return end
-    TriggerServerEvent('qb-taxijob:server:pickupNPC', currentBenchIndex)
+    if inPickupZone and currentPickupBenchIndex then
+        my_webui:SendEvent('toggleMeter')
+        TriggerServerEvent('qb-taxijob:server:pickupNPC', currentPickupBenchIndex)
+    elseif inDropoffZone and currentDropoffBenchIndex then
+        my_webui:SendEvent('resetMeter')
+        TriggerServerEvent('qb-taxijob:server:dropoffNPC', currentDropoffBenchIndex)
+    end
 end)
--- qb-taxijob/html/script.js
+++ qb-taxijob/html/script.js
@@ -1,53 +1,54 @@
-let meterStarted = false;
+document.addEventListener("DOMContentLoaded", function () {
+    let meterStarted = false;
 
-const updateMeter = (meterData) => {
-    $("#total-price").html("$ " + meterData.currentFare.toFixed(2));
-    $("#total-distance").html(meterData.distanceTraveled.toFixed(2) + " mi");
-};
+    const updateMeter = (meterData) => {
+        $("#total-price").html("$ " + meterData.currentFare.toFixed(2));
+        $("#total-distance").html(meterData.distanceTraveled.toFixed(2) + " mi");
+    };
 
-const resetMeter = () => {
-    $("#total-price").html("$ 0.00");
-    $("#total-distance").html("0.00 mi");
-};
+    const resetMeter = () => {
+        $("#total-price").html("$ 0.00");
+        $("#total-distance").html("0.00 mi");
+    };
 
-const toggleMeter = (enabled) => {
-    if (enabled) {
-        $(".toggle-meter-btn").html("<p>Started</p>");
-        $(".toggle-meter-btn p").css({ color: "rgb(51, 160, 37)" });
-    } else {
-        $(".toggle-meter-btn").html("<p>Stopped</p>");
-        $(".toggle-meter-btn p").css({ color: "rgb(231, 30, 37)" });
-    }
-};
+    const toggleMeter = (enabled) => {
+        if (enabled) {
+            $(".toggle-meter-btn").html("<p>Started</p>");
+            $(".toggle-meter-btn p").css({ color: "rgb(51, 160, 37)" });
+        } else {
+            $(".toggle-meter-btn").html("<p>Stopped</p>");
+            $(".toggle-meter-btn p").css({ color: "rgb(231, 30, 37)" });
+        }
+    };
 
-const meterToggle = () => {
-    if (!meterStarted) {
-        hEvent("enableMeter", JSON.stringify({ enabled: true }));
-        toggleMeter(true);
-        meterStarted = true;
-    } else {
-        hEvent("enableMeter", JSON.stringify({ enabled: false }));
-        toggleMeter(false);
-        meterStarted = false;
-    }
-};
+    const meterToggle = () => {
+        if (!meterStarted) {
+            hEvent("enableMeter", JSON.stringify({ enabled: true }));
+            toggleMeter(true);
+            meterStarted = true;
+        } else {
+            hEvent("enableMeter", JSON.stringify({ enabled: false }));
+            toggleMeter(false);
+            meterStarted = false;
+        }
+    };
 
-const openMeter = (meterData) => {
-    $(".container").fadeIn(150);
-    $("#total-price-per-100m").html("$ " + meterData.defaultPrice.toFixed(2));
-};
+    const openMeter = (meterData) => {
+        $(".container").fadeIn(150);
+        $("#total-price-per-100m").html("$ " + meterData.defaultPrice.toFixed(2));
+    };
 
-const closeMeter = () => {
-    $(".container").fadeOut(150);
-};
+    const closeMeter = () => {
+        $(".container").fadeOut(150);
+    };
 
-$(document).ready(function () {
-    window.addEventListener("message", (event) => {
+    window.addEventListener("message", function (event) {
         const eventData = event.data;
+        const eventArgs = event.data.args[0];
         switch (eventData.name) {
             case "openMeter":
-                if (eventData.toggle) {
-                    openMeter(eventData.meterData);
+                if (eventArgs.toggle) {
+                    openMeter(eventArgs.meterData);
                 } else {
                     closeMeter();
                 }
@@ -56,7 +57,7 @@ $(document).ready(function () {
                 meterToggle();
                 break;
             case "updateMeter":
-                updateMeter(eventData.meterData);
+                updateMeter(eventArgs.meterData);
                 break;
             case "resetMeter":
                 resetMeter();
